c219bd6dac157e82b71a4ec1032db3bc9fd2bdc3
Jackson Yin <Yrh7383111@outlook.com>
2020-07-29 16:34:29 -0400

    Complete to fetch the data from web

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component
+{
+    // Constructor
+    constructor()
+    {
+        super();
+
+        this.state = {
+            monsters: []
+        };
+    }
+
+
+    // Fetch the data when the page first gets rendered
+    componentDidMount()
+    {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ monsters: users }));
+    }
+
+
+    // Rendering
+    render()
+    {
+        return (
+            <div className='App'>
+                {this.state.monsters.map(monster => (
+                    <h1>{monster.name}</h1>
+                ))}
+            </div>
+        );
+    }
 }
+
 
 export default App;
